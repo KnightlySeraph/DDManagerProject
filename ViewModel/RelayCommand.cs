16fbebd5efb8652c077f17756ac109ae8f1ff549
KnightlySeraph <justin424bryant@gmail.com>
2021-03-26 22:33:07 -0500

    [Addition] Created RelayCommand class for Command Binding to ViewModel layer.

---
+++ ViewModel/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DDManagerSolution.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        Action<object> _executeMethod;
+        Func<object, bool> _canExecuteMethod;
+        public RelayCommand(Action<object> executeMethod, Func<object, bool> canExecuteMethod)
+        {
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return (_canExecuteMethod != null);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeMethod(parameter);
+        }      
+    }
+}
