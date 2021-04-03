dbe43103bb3bb12d508c0f7f1897ba5ce5f6f4a3
KnightlySeraph <justin424bryant@gmail.com>
2021-04-02 21:34:44 -0500

    [Addition] Added visibility IValueConverters

--- View/Converters/BasicConverters.cs
+++ View/Converters/BasicConverters.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace DDManagerSolution
@@ -12,12 +13,45 @@ namespace DDManagerSolution
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is bool)
+            {
+                return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is Visibility)
+            {
+                return (Visibility)value == Visibility.Visible;
+            }
+
+            return false;
+        }
+    }
+
+    public class BoolToInvVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility)
+            {
+                return (Visibility)value != Visibility.Visible;
+            }
+
+            return false;
         }
     }
 }
