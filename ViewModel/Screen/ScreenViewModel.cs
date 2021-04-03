034e9b46d76895fcf744445e3085a412b90d8fcb
KnightlySeraph <justin424bryant@gmail.com>
2021-04-02 21:35:11 -0500

    [Addition] Screen Framework. Screen is equivalent to a workspace.

---
+++ ViewModel/Screen/ScreenViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DDManagerSolution.Model;
+
+namespace DDManagerSolution.ViewModel
+{
+    public class ScreenViewModel
+    {
+        private Screen _screen;
+
+        public ScreenViewModel(Screen screen)
+        {
+            _screen = screen;
+        }
+    }
+}
