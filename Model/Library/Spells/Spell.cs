a711a102a1951113b2f8216036b8d63ebe5604ac
bryantj2287 <bryantj2287@my.uwstout.edu>
2021-11-25 23:25:02 -0600

    [Addition] Added a Spell and Spell List model classes.

---
+++ Model/Library/Spells/Spell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDManagerSolution.Model
+{
+    public class Spell
+    {
+        public Spell()
+        {
+
+        }
+
+        private int         _level;
+        private string      _name;
+        private string      _school;
+        private string      _castTime;
+        private string      _range;
+        private string      _desc;
+        
+    }
+}
---
+++ Model/Library/Spells/SpellSheet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDManagerSolution.Model
+{
+    public class SpellSheet
+    {
+        public SpellSheet(bool generateNewCollections = true)
+        {
+            if (generateNewCollections)
+                _initializeSpellCollections();
+        }
+
+        private void _initializeSpellCollections()
+        {
+            _cantrips       = new List<Spell>();
+            _first          = new List<Spell>();
+            _second         = new List<Spell>();
+            _third          = new List<Spell>();
+            _fourth         = new List<Spell>();
+            _fifth          = new List<Spell>();
+            _sixth          = new List<Spell>();
+            _seventh        = new List<Spell>();
+            _eightth        = new List<Spell>();
+            _ninth          = new List<Spell>();
+        }
+
+        private List<Spell> _cantrips;
+        private List<Spell> _first;
+        private List<Spell> _second;
+        private List<Spell> _third;
+        private List<Spell> _fourth;
+        private List<Spell> _fifth;
+        private List<Spell> _sixth;
+        private List<Spell> _seventh;
+        private List<Spell> _eightth;
+        private List<Spell> _ninth;
+    }
+}
